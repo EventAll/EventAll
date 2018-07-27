04c3a6089231fd1de9b138e3d1733b135697a6d9
peniqliotuv <jerrytsu@usc.edu>
2018-07-27 07:47:25 -0700

    Oops I broke master with a merge...

--- src/app/index.js
+++ src/app/index.js
@@ -1,34 +1,38 @@
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
 
-import TimelineScreen from '../Timeline/components/Timeline';
 import SettingsScreen from '../Settings/components/Settings';
 import EventPageContainer from '../EventPage/screens/EventPageContainer';
 
 import LoginScreen from '../Login/components/Login';
 import SignupScreen from '../Signup/components/Signup';
 
-const HomeNavigator = createBottomTabNavigator({
-  Timeline: { screen: TimelineScreen },
-  Event: { screen: EventPageContainer },
-  Settings: { screen: SettingsScreen },
-},
-{
-  initialRouteName: 'Event',
-});
-
-const RootNavigator = createStackNavigator({
-  Login: { screen: LoginScreen },
-  Signup: { screen: SignupScreen },
-  Home: { screen: HomeNavigator },
-},
-{
-  navigationOptions: {
-    headerLeft: null,
+const HomeNavigator = createBottomTabNavigator(
+  {
+    Event: { screen: EventPageContainer },
+    Settings: { screen: SettingsScreen },
   },
-},
-{
-  initialRouteName: 'Login',
-});
+  {
+    initialRouteName: 'Event',
+  }
+);
+
+const RootNavigator = createStackNavigator(
+  {
+    Login: { screen: LoginScreen },
+    Signup: { screen: SignupScreen },
+    Home: { screen: HomeNavigator },
+  },
+  {
+    navigationOptions: {
+      headerLeft: null,
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
 
 export default RootNavigator;
-
