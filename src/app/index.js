4736ae3bf9469014fad3a5b3f082473ad0847086
peniqliotuv <jerrytsu@usc.edu>
2018-07-28 09:39:52 -0700

    Moved everything to event folder

--- src/app/index.js
+++ src/app/index.js
@@ -1,60 +1,10 @@
-import React from 'react';
-import {
-  createBottomTabNavigator,
-  createStackNavigator,
-} from 'react-navigation';
-import PropTypes from 'prop-types';
-import { Ionicons } from '@expo/vector-icons';
+import { createStackNavigator } from 'react-navigation';
 
-import colors from '../globals/colors';
-import SettingsScreen from '../Settings/screens/SettingsScreen';
-import EventPageScreen from '../EventPage/screens/EventPageScreen';
 import HomeScreen from '../Home/screens/HomeScreen';
-import ScheduleContainer from '../Schedule/screens/ScheduleContainer';
 
 import LoginScreen from '../Login/screens/LoginScreen';
 import SignupScreen from '../Signup/screens/SignupScreen';
-
-const EventNavigator = createBottomTabNavigator(
-  {
-    Schedule: { screen: ScheduleContainer },
-    Event: { screen: EventPageScreen },
-    Settings: { screen: SettingsScreen },
-  },
-  {
-    initialRouteName: 'Event',
-    navigationOptions: ({ navigation }) => {
-      const tabBarIcon = ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === 'Settings') {
-          iconName = 'ios-settings';
-        }
-        else if (routeName === 'Event') {
-          iconName = 'md-calendar';
-        }
-        else if (routeName === 'Schedule') {
-          iconName = 'ios-compass-outline';
-        }
-        return (
-          <Ionicons
-            name={iconName}
-            size={35}
-            color={focused ? colors.userOrange : tintColor}
-          />
-        );
-      };
-      tabBarIcon.propTypes = {
-        focused: PropTypes.bool.isRequired,
-        tintColor: PropTypes.string.isRequired,
-      };
-      // This code below is necessary for rendering the icons at the bottom of the tab
-      return {
-        tabBarIcon,
-      };
-    },
-  }
-);
+import EventNavigator from '../Event/navigator';
 
 const RootNavigator = createStackNavigator(
   {
