34dcaae7c1f919f532f9402f85569fb7ed407d90
Samantha I-Ning Chang <iningcha@usc.edu>
2018-08-17 14:32:38 -0700

    connected login with backend (#9)

--- src/Authentication/screens/LoginScreen/index.js
+++ src/Authentication/screens/LoginScreen/index.js
@@ -1,98 +1,36 @@
-import React, { Component } from 'react';
-import { Image, View, Text, TouchableOpacity, TextInput } from 'react-native';
+import React from 'react';
+import { Alert, AsyncStorage } from 'react-native';
+import { Mutation } from 'react-apollo';
 import PropTypes from 'prop-types';
+import Login from '../../components/Login';
+import { LOGIN } from '../../../graphql/mutations';
 
-import styles from './styles';
+const LoginScreen = ({ navigation }) => (
+  <Mutation mutation={LOGIN}>
+    {(login, { data, loading, error }) => {
+      if (error) {
+        Alert.alert('There was an error logging in.');
+      }
+      else if (data) {
+        const { token } = data.login;
+        AsyncStorage.setItem('token', token).then(() => navigation.navigate('Home'));
+      }
 
-const logoImg = require('../../../../assets/EventAllLightLong.png');
+      return (
+        <Login
+          navigation={navigation}
+          dispatchMutation={login}
+          loading={loading}
+        />
+      );
+    }}
+  </Mutation>
+);
 
-class LoginScreen extends Component {
-  static propTypes = {
-    navigation: PropTypes.shape({
-      navigate: PropTypes.func.isRequired,
-    }).isRequired,
-  };
-
-  state = {
-    username: '',
-    password: '',
-  };
-
-  verifyFBLogin = () => {
-    // Facebook authentication here
-  };
-
-  verifyLogin = () => {
-    if (this.state.username === '') {
-      // error message
-      return;
-    }
-
-    if (this.state.password === '') {
-      // error message
-      return;
-    }
-
-    // make sure credentials match ones from database
-
-    this.props.navigation.navigate('Home');
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image style={styles.logo} source={logoImg} />
-
-        <TouchableOpacity
-          style={styles.FBLoginButton}
-          activeOpacity={0.7}
-          onPress={this.verifyFBLogin}
-        >
-          <Text style={styles.buttonText}>Log In With Facebook</Text>
-        </TouchableOpacity>
-
-        <View style={styles.rowContainer}>
-          <View style={styles.line} />
-          <Text style={styles.or}>OR</Text>
-          <View style={styles.line} />
-        </View>
-
-        <Text style={styles.regularText}>Log in to your account</Text>
-
-        <View style={styles.formContainer}>
-          <TextInput
-            placeholder="Username or Email"
-            style={styles.formField}
-            underlineColorAndroid="transparent"
-            onChangeText={(username) => this.setState({ username })}
-          />
-
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            style={styles.formField}
-            underlineColorAndroid="transparent"
-            onChangeText={(password) => this.setState({ password })}
-          />
-        </View>
-
-        <TouchableOpacity
-          style={styles.loginButton}
-          activeOpacity={0.7}
-          onPress={this.verifyLogin}
-        >
-          <Text style={styles.buttonText}>Log In</Text>
-        </TouchableOpacity>
-
-        <Text
-          style={styles.signUpText}
-          onPress={() => this.props.navigation.navigate('Signup')}
-        >
-          Don&apos;t have an account? Sign up
-        </Text>
-      </View>
-    );
-  }
-}
+LoginScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default LoginScreen;
